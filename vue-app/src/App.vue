d509b8e63a67cf257015b3e2e454cb17399f90c5
leonawolff <leona.wolff.ok@gmail.com>
2020-12-19 19:59:58 +0000

    Update App.vue

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,34 +1,23 @@
-
 <template>
-  <div id="app">
-    <img alt="frog" src="./assets/Frog.gif">
-    <h1>GitHub API Visualiser</h1>
-    <form>
-      <label for="fname">Github username:</label><br/><br/>
-      <input type="text" id="username" name="username"><br/><br/>
-      <input type="submit" value="Go!">
-    </form>
-  </div>
+  <v-app>
+    <v-main>
+      <Main/>
+    </v-main>
+  </v-app>
 </template>
 
-
-
-
-
 <script>
+import Main from './components/Main';
+
 export default {
-  name: 'App'
-}
+  name: 'App',
+
+  components: {
+    Main,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
