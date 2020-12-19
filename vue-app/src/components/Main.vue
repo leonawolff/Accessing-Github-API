e249ee73705628ca5e566a279f736502bbce4c0b
leonawolff <leona.wolff.ok@gmail.com>
2020-12-19 22:02:51 +0000

    Update Main.vue

--- vue-app/src/components/Main.vue
+++ vue-app/src/components/Main.vue
@@ -1,3 +1,5 @@
+Vue.prototype.$username = null
+
 <template>
     <v-app>
         <v-main>
@@ -6,23 +8,54 @@
                     <img alt="frog" src="../assets/Frog.gif">
                 </v-card>
                 <br/><br/>
+
+
                 <v-card color="purple lighten-4" align="center">
-                    <br/><br/>
-                    <h1>GitHub API Visualiser</h1>
-                    <br/><br/>
-                    <v-text-field
-                        v-model="name"
-                        :rules="nameRules"
-                        label="GitHub Username"
-                        outlined
-                    >
-                    </v-text-field>
-                    <v-btn
-                        color="purple lighten-2"
-                        elevation="4" >
-                        Go!
-                    </v-btn>
-                    <br/><br/>
+                    <v-card-text> 
+                        <br/><br/>
+                        <h1>GitHub API Visualiser</h1>
+                        <br/><br/>
+                            <v-text-field
+                                v-model="name"
+                                :rules="nameRules"
+                                label="GitHub Username"
+                                outlined
+                            >
+                            </v-text-field>
+                        </v-card-text> 
+                    
+                        <v-card-actions>
+                            <v-btn
+                                color="purple lighten-2"
+                                elevation="4"
+                                @click="reveal = true" >
+                                Go!
+                            </v-btn>
+                        </v-card-actions>
+
+                        <v-expand-transition>
+                            <v-card
+                                v-if="reveal"
+                                class="transition-fast-in-fast-out v-card--reveal"
+                                style="height: 100%;"
+                            >
+                                <v-card-text> 
+                                    <br/><br/>
+                                    <h1>GitHub API Visualiser</h1>
+                                    <br/><br/>
+                                    <div>Sum Shit</div>
+                                </v-card-text> 
+                    
+                                <v-card-actions>
+                                    <v-btn
+                                        color="purple lighten-2"
+                                        elevation="4"
+                                        @click="reveal = false" >
+                                        Close
+                                    </v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-expand-transition>
                 </v-card>
             </v-container>
         </v-main>
@@ -30,8 +63,7 @@
 </template>
 
 <script>
-export default {
-  name: 'Main'
-}
+    export default {
+        name: 'Main'
+    }
 </script>
-
